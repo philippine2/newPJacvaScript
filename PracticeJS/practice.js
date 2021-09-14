fb6db850186821da20eee5873a9f646ef972cd40
philippine2 <49458817+philippine2@users.noreply.github.com>
2021-09-14 14:02:47 -0700

    Add files via upload

---
+++ PracticeJS/practice.js
@@ -0,0 +1,86 @@
+let firstName = 'Lata';
+
+// which value has x;
+lex = 'Geeta';
+console.log(x);
+// x is geeta 
+
+//decare 2 variables.
+let flower = 'rose';
+let tree = 'maple';
+
+//Which value does x have after execution of the following code?
+let x = 'Tic';
+x = 'Tac';
+x = 'Toe';
+// "Toe"
+
+// Which value does x have after execution of the following code?
+let x = 'Laurel';
+let y = 'Hardy';
+let z = y;
+y = x;
+x = z;
+// Hardy
+
+//
+//Define a function hello that returns 'Hello world!'.
+function hello () {
+    return 'Hello world!';
+    
+    }
+
+    //Define two functions. The first function a should return 'Hello a!' and the second function b should return 'Hello b!'.
+
+    function a (){
+        return 'Hello a!';
+        }
+        function b () {
+        return 'Hello b!';
+        }
+//1. Define a function greet returning the value 'Haydo!'.
+//2. Declare a variable salutation. Call the function greet and assign the result of the call to the variable salutation
+
+function greet() {
+    return 'Haydo!';
+  }
+  
+  let salutation = greet();
+  //function greet() {
+  return 'Haydo!';
+
+  //Which value does x have after execution of the following code?
+  function hello() {
+    return 'Hi!';
+  }
+  
+  let x = hello();
+
+  // 'Hi!'
+
+  // Write a function greet having one parameter and returning 
+  - Solution
+function greet(name) {
+  return 'Hello ' + name + '!';
+}
+// Which value does x have after execution of the following code?
+
+function hi(name) {
+    return 'Hi ' + name + '!';
+  }
+  
+  let h1 = hi('Selva');
+  let h2 = hi('Pola');
+  let x = h1 + ' ' + h2;
+
+  // Hi Selva! Hi Pola!
+
+  //Write a function log that logs 'Hello Console!'. If you are working with a desktop browser, open the developer tools to see the output there as well.
+  function log() {
+    console.log('Hello Console!');
+  }
+
+  
+
+
+
